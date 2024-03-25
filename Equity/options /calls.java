ab6515836d07b01f854521e6137f18495010bcb2
Dhruv Patel <dhruvj1323@gmail.com>
2024-03-25 18:30:08 -0400

    First commit; Creating a project to price derivatives. Aim:  To use pricing models and APIs to back-test. Implemented in Python & Java

---
+++ Equity/options /calls.java
@@ -0,0 +1,34 @@
+public class calls{
+    protected boolean isEuropean;
+    protected double strikePrice;
+
+    protected double expiryDate;
+    
+    /**
+     * @param S Current Stock Price
+     * @return
+     */
+    public double callPrice(double S, double K ){
+        //FIXME
+        return 0;
+
+
+
+    }
+
+    public double d1(){
+        //FIXME
+        return 0;
+
+    }
+
+    public double d2(){
+        //FIXME
+        return 0;
+
+
+    }
+
+
+
+}
---
+++ Equity/stock.java
@@ -0,0 +1,15 @@
+package Equity;
+
+public class stock {
+
+
+    /**
+     * 
+     * @param TICKER The ticker of the stock 
+     */
+    public stock(String TICKER){
+
+    }
+    
+
+}
